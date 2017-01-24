41a400db10896ceda27820869550c23667eff064
MattWZA <matthewwalker101@gmail.com>
2017-01-24 22:26:29 +0200

    oop intro

---
+++ shawpify.localhost/scripts/product.js
@@ -0,0 +1,17 @@
+(function(){
+	
+	var Product = function(pName, pCode, pPrice){
+		this.name = pName;
+		this.code = pCode;
+		this.price = pPrice;
+
+		
+	}
+	Product.prototype.getPrice = function(){
+		return "$" + this.price;
+	}
+	var aProduct = new Product("Bluetooth speaker", "BS1001", 99.95);
+			
+	document.write(aProduct.getPrice());
+
+})()
